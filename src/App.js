dcfafaf91d936c131d50652be9c7014a5fe3bda3
Lucas Rama <lucasrama2002@gmail.com>
2025-04-16 09:43:26 -0300

    estruturando o projeto adicionando as routes, components base e layouts base

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+      </div>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exatc path="/" element={<Home />}/>
+          <Route path="/company" element={<Company />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="/newproject" element={<NewProject />}/>
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
