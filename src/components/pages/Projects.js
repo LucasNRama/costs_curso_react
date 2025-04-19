93abedc7daf3a2bb825985d7b6055f779bf65d1c
Lucas Rama <lucasrama2002@gmail.com>
2025-04-19 10:54:28 -0300

    Criando componente de loader

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,12 +4,14 @@ import styles from './Project.module.css'
 
 import Message from '../layout/Message'
 import Container from '../layout/Container'
+import Loading from '../layout/Loading'
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
 
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -18,18 +20,20 @@ function Projects() {
     }
 
     useEffect(() => {
-
-        fetch('http://localhost:5000/projects',{
-            method: 'GET',
-            headers: {
-                'Content-Type' : 'application/json'
-            }
-        }).then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            setProjects(data)
-        })
-        .catch((err) => console.log(err))
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects',{
+                method: 'GET',
+                headers: {
+                    'Content-Type' : 'application/json'
+                }
+            }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+                setRemoveLoading(true)
+            })
+            .catch((err) => console.log(err))    
+        }, 1000)
     }, [])
 
     return(
@@ -46,10 +50,14 @@ function Projects() {
                         name={project.name}
                         id={project.id}
                         budget={project.budget}
-                        category={project.category?.name}
+                        category={project.category?.name} 
                         key={project.id}
                     />
                 )}
+            {!removeLoading && <Loading />}
+            {removeLoading && projects.length === 0 &&(
+                <p>Não há projetos cadastrados!</p>
+            )}
             </Container>
         </div>
     )
